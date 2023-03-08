528b65e527a3efec9d7413762cb10917d9c9cf6f
erick-sudo <erickochieng766@gmail.com>
2023-03-08 23:59:49 +0300

    Back

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import h1 from "../assets/gallery/horus1.jpg"
 import h2 from "../assets/gallery/horus2.jpg"
 import h3 from "../assets/gallery/horus3.jpg"
@@ -14,9 +14,13 @@ function Home() {
 function SlideShow() {
 
     const slides = [h1, h2, h3, h1, h2, h3]
-    const [index, setIndex] = useState(2)
+    const [index, setIndex] = useState(3)
+
+    const slideRapperRef = useRef()
+    const floatRef = useRef()
 
     useEffect(() => {
+        slideRapperRef.current.style.height = (floatRef.current.getBoundingClientRect().height*1.3)+"px"
         // setInterval(() =>{
         //     setIndex(idx => (idx + 1)%6)
         // },5000)
@@ -25,12 +29,14 @@ function SlideShow() {
     return (
         <div className="slide-show-wrapper">
             <div className="slide-show">
-                <div className="slide" key={index}>
+                <div ref={slideRapperRef} className="slide" key={index}>
                     <img src={slides[index]} alt="Slide" />
-                    <div className="float-description">
+                    <div ref={floatRef} className="float-description">
                         <h1>FREIGHT FORWARDING</h1>
                         <h2>WHEREVER IT NEEDS TO GO</h2>
+                        <div className="slide-line"></div>
                         <h4>YOUR SOURCE FOR LOGISTICS FOR MORE THAN 20 YEARS</h4>
+                        <button className="contact-us">Contact Us</button>
                     </div>
                 </div>
             </div>
